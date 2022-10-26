241b9405927bbd188cd64a1fe128b284606c542c
Nguyễn Minh Hưng <90996598+minhunsocute@users.noreply.github.com>
2022-10-26 12:06:17 +0700

    setup folder

---
+++ lib/routes/route_name.dart
@@ -0,0 +1,30 @@
+class RouteNames {
+  static const String splashScreen = "/splash_screen";
+  static const String introScreen = "/intro_screen";
+  static const String signInScreen = "/sign_in_screen";
+  static const String dashboardScreen = "/dashboard_screen";
+  static const String notificationScreen = "/notification_screen";
+  static const String favoriteScreen = "/favorite_screen";
+  static const String searchScreen = "/search_screen";
+  static const String doctorDetailScreen = "/doctor_detail_screen";
+  static const String viewReiviewScreen = "/view_reiview_screen";
+  static const String bookAppointmentScreen = "/book_appointment_screen";
+
+  static const String addNewCardScreen = "/add_new_card_screen";
+  static const String enterPinScreen = "/enter_pin_screen";
+  static const String patientDetailScreen = "/patient_detail_screen";
+  static const String reviewSumaryScreen = "/review_sumary_screen";
+  static const String selectPackageScreen = "/select_package_screen";
+  static const String payementScreen = "/payement_screen";
+
+  static const String reasonScheduleChangeScreen =
+      "/reason_schedule_change_screen";
+
+  static const String myAppointmentDetailScreen =
+      "/my_appointment_detail_screen";
+  static const String messageScreen = "/message_screen";
+  static const String successfulScreen = "/successful_screen";
+  static const String writeReviewScreen = "/write_review_screen";
+  static const String callScreen = "/call_screen";
+  static const String callRecordScreen = "/call_record_screen";
+}
